bbe1905f45c5647ecfb6b8a5edeaf812d71ecebc
Lukas Spiss <lukas@spiss.dev>
2025-12-03 19:07:38 +0000

    solve day 3 puzzle 1

--- day03/solve.go
+++ day03/solve.go
@@ -1,8 +1,30 @@
 package day03
 
-func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+import (
+	"strconv"
+	"strings"
+)
+
+func SolvePuzzle1(input string) (result int) {
+	lines := strings.SplitSeq(input, "\n")
+
+	for line := range lines {
+		var first, second int
+		for i, c := range line {
+			current, _ := strconv.Atoi(string(c))
+			if current > first && i < len(line)-1 {
+				first = current
+				second = 0
+			} else if current > second {
+				second = current
+			}
+		}
+
+		joltage, _ := strconv.Atoi(strconv.Itoa(first) + strconv.Itoa(second))
+		result += joltage
+	}
+
+	return result
 }
 
 func SolvePuzzle2(input string) int {
