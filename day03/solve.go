fdbf806ed63ea82d9d731b2f41b6a1238066542d
Lukas Spiss <lukas@spiss.dev>
2025-12-03 20:02:09 +0000

    solve day 3 puzzle 2

--- day03/solve.go
+++ day03/solve.go
@@ -27,7 +27,48 @@ func SolvePuzzle1(input string) (result int) {
 	return result
 }
 
-func SolvePuzzle2(input string) int {
-	// TODO: solve puzzle 2
-	return 0
+func SolvePuzzle2(input string) (result int) {
+	lines := strings.SplitSeq(input, "\n")
+	length := 12
+
+	for line := range lines {
+		batteries := make([]int, length)
+		for charIndex, c := range line {
+			current, _ := strconv.Atoi(string(c))
+			reset := false
+			for batteryIndex, battery := range batteries {
+				if reset {
+					if battery == 0 {
+						break
+					}
+					batteries[batteryIndex] = 0
+					continue
+				}
+
+				if battery == 0 {
+					batteries[batteryIndex] = current
+					break
+				}
+
+				if current > battery {
+					if charIndex < len(line)-length+batteryIndex+1 {
+						batteries[batteryIndex] = current
+						reset = true
+					} else if batteryIndex == length-1 {
+						batteries[batteryIndex] = current
+					}
+				}
+			}
+		}
+
+		var joltageStr string
+		for _, battery := range batteries {
+			joltageStr += strconv.Itoa(battery)
+		}
+
+		joltage, _ := strconv.Atoi(joltageStr)
+		result += joltage
+	}
+
+	return result
 }
