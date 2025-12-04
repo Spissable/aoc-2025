b3929bfe174b40da339fdd3235b30dfb96b32286
Lukas Spiss <lukas@spiss.dev>
2025-12-04 20:33:59 +0000

    solve day 4 puzzle 1

--- day04/solve.go
+++ day04/solve.go
@@ -1,11 +1,85 @@
 package day04
 
-func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+import (
+	"strings"
+)
+
+func SolvePuzzle1(input string) (result int) {
+	p := newPapers(input)
+
+	for coords, _ := range p {
+		if len(p.getNeighbors(coords.x, coords.y)) < 4 {
+			result++
+		}
+	}
+
+	return result
 }
 
 func SolvePuzzle2(input string) int {
 	// TODO: solve puzzle 2
 	return 0
+}
+
+type coord struct{ x, y int }
+type papers map[coord]bool
+
+func newPapers(input string) papers {
+	result := make(map[coord]bool)
+	lines := strings.Split(input, "\n")
+	for y, line := range lines {
+		for x, c := range line {
+			if c == '@' {
+				result[coord{x: x, y: y}] = true
+			}
+		}
+	}
+
+	return result
+}
+
+func (p papers) getNeighbors(x, y int) (result []coord) {
+	for _, c := range neighborCombinations {
+		neighbor := coord{x: x + c.x, y: y + c.y}
+		if p[neighbor] {
+			result = append(result, neighbor)
+		}
+	}
+
+	return result
+}
+
+var neighborCombinations = []coord{
+	{
+		x: 0,
+		y: 1,
+	},
+	{
+		x: 1,
+		y: 0,
+	},
+	{
+		x: 1,
+		y: 1,
+	},
+	{
+		x: 0,
+		y: -1,
+	},
+	{
+		x: -1,
+		y: 0,
+	},
+	{
+		x: -1,
+		y: -1,
+	},
+	{
+		x: -1,
+		y: 1,
+	},
+	{
+		x: 1,
+		y: -1,
+	},
 }
