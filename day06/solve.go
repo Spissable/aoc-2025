a5dd13586256abc5436c4cedbff6293348fc2115
Lukas Spiss <lukas@spiss.dev>
2025-12-06 11:09:11 +0000

    solve day 6 puzzle 2

--- day06/solve.go
+++ day06/solve.go
@@ -23,9 +23,22 @@ func SolvePuzzle1(input string) (result int) {
 	return result
 }
 
-func SolvePuzzle2(input string) int {
-	// TODO: solve puzzle 2
-	return 0
+func SolvePuzzle2(input string) (result int) {
+	c := newCalc2(input)
+	for _, calc := range c {
+		subTotal := calc.nums[0]
+		for _, num := range calc.nums[1:] {
+			if calc.add {
+				subTotal += num
+			} else {
+				subTotal *= num
+			}
+		}
+
+		result += subTotal
+	}
+
+	return result
 }
 
 type eval struct {
@@ -50,6 +63,88 @@ func newCalc(input string) (result []eval) {
 			}
 
 			result[elemI].nums = append(result[elemI].nums, num)
+		}
+	}
+
+	return result
+}
+
+func newCalc2(input string) (result []eval) {
+	lines := strings.Split(input, "\n")
+	if len(lines) == 0 {
+		return result
+	}
+
+	maxLen := 0
+	for _, line := range lines {
+		if len(line) > maxLen {
+			maxLen = len(line)
+		}
+	}
+
+	columnRanges := [][]int{}
+
+	inColumn := false
+	start := 0
+
+	for pos := 0; pos < maxLen; pos++ {
+		hasContent := false
+		for _, line := range lines {
+			if pos < len(line) && line[pos] != ' ' {
+				hasContent = true
+				break
+			}
+		}
+
+		if hasContent && !inColumn {
+			start = pos
+			inColumn = true
+		} else if !hasContent && inColumn {
+			columnRanges = append(columnRanges, []int{start, pos})
+			inColumn = false
+		}
+	}
+
+	if inColumn {
+		columnRanges = append(columnRanges, []int{start, maxLen})
+	}
+
+	for colIdx := len(columnRanges) - 1; colIdx >= 0; colIdx-- {
+		colRange := columnRanges[colIdx]
+		startPos := colRange[0]
+		endPos := colRange[1]
+
+		eval := eval{}
+
+		for pos := endPos - 1; pos >= startPos; pos-- {
+			var digits []string
+			var operator string
+
+			for _, line := range lines {
+				if pos < len(line) && line[pos] != ' ' {
+					char := string(line[pos])
+					if char == "+" || char == "*" {
+						operator = char
+					} else {
+						digits = append(digits, char)
+					}
+				}
+			}
+
+			if len(digits) > 0 {
+				numberStr := strings.Join(digits, "")
+				if num, err := strconv.Atoi(numberStr); err == nil {
+					eval.nums = append(eval.nums, num)
+				}
+			}
+
+			if operator != "" {
+				eval.add = operator == "+"
+			}
+		}
+
+		if len(eval.nums) > 0 {
+			result = append(result, eval)
 		}
 	}
 
