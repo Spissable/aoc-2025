0879cb2d6a30865c5e325a9de25a12016f232012
Lukas Spiss <lukas@spiss.dev>
2025-12-09 16:48:44 +0000

    solve day 9 puzzle 2

--- day09/solve.go
+++ day09/solve.go
@@ -16,7 +16,24 @@ func SolvePuzzle1(input string) int {
 }
 
 func SolvePuzzle2(input string) int {
-	// TODO: solve puzzle 2
+	coords := newCoords(input)
+	pairs := newPairs(coords)
+	sort.Slice(pairs, func(a, b int) bool {
+		return pairs[a].area > pairs[b].area
+	})
+	edges := make([]coord, 0)
+	edges = append(edges, coords...)
+	coords = append(coords, coords[0])
+	for i, p := range coords[:len(coords)-1] {
+		edges = append(edges, getEdges(p, coords[i+1])...)
+	}
+
+	for j := range pairs {
+		if isValid(pairs[j], edges) {
+			return pairs[j].area
+		}
+	}
+
 	return 0
 }
 
@@ -43,24 +60,14 @@ func newCoords(input string) (result []coord) {
 }
 
 func getArea(a, b coord) int {
-	x := a.x - b.x + 1
-	if x < 0 {
-		x *= -1
-	}
-
-	y := a.y - b.y + 1
-	if y < 0 {
-		y *= -1
-	}
-
-	return x * y
+	return (max(a.x, b.x) - min(a.x, b.x) + 1) * (max(a.y, b.y) - min(a.y, b.y) + 1)
 }
 
-func newPairs(points []coord) (result []pair) {
-	for i := range points {
-		for j := i + 1; j < len(points); j++ {
-			a := points[i]
-			b := points[j]
+func newPairs(coords []coord) (result []pair) {
+	for i := range coords {
+		for j := i + 1; j < len(coords); j++ {
+			a := coords[i]
+			b := coords[j]
 			result = append(result, pair{
 				a:    a,
 				b:    b,
@@ -70,4 +77,45 @@ func newPairs(points []coord) (result []pair) {
 	}
 
 	return result
+}
+
+func i(p pair, e coord) bool {
+	return e.x < max(p.a.x, p.b.x) && e.x > min(p.a.x, p.b.x) && e.y < max(p.a.y, p.b.y) && e.y > min(p.a.y, p.b.y)
+}
+
+func getEdges(a, b coord) (c []coord) {
+	if a.x == b.x {
+		for i := min(a.y, b.y) + 1; i < max(a.y, b.y); i++ {
+			c = append(c, coord{a.x, i})
+		}
+		return c
+	}
+	for i := min(a.x, b.x) + 1; i < max(a.x, b.x); i++ {
+		c = append(c, coord{i, a.y})
+	}
+	return c
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func isValid(p pair, edges []coord) bool {
+	for _, e := range edges {
+		if e.x < max(p.a.x, p.b.x) && e.x > min(p.a.x, p.b.x) && e.y < max(p.a.y, p.b.y) && e.y > min(p.a.y, p.b.y) {
+			return false
+		}
+	}
+
+	return true
 }
