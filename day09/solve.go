15c33e3bc94db1d839fbb4a842e7164ca70ce5d0
Lukas Spiss <lukas@spiss.dev>
2025-12-09 09:16:26 +0000

    solve day 9 puzzle 1

--- day09/solve.go
+++ day09/solve.go
@@ -1,11 +1,73 @@
 package day09
 
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
 func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+	pairs := newPairs(newCoords(input))
+	sort.Slice(pairs, func(a, b int) bool {
+		return pairs[a].area > pairs[b].area
+	})
+
+	return pairs[0].area
 }
 
 func SolvePuzzle2(input string) int {
 	// TODO: solve puzzle 2
 	return 0
+}
+
+type coord struct {
+	x, y int
+}
+
+type pair struct {
+	a, b coord
+	area int
+}
+
+func newCoords(input string) (result []coord) {
+	lines := strings.SplitSeq(input, "\n")
+	for line := range lines {
+		fields := strings.Split(line, ",")
+		x, _ := strconv.Atoi(fields[0])
+		y, _ := strconv.Atoi(fields[1])
+
+		result = append(result, coord{x: x, y: y})
+	}
+
+	return result
+}
+
+func getArea(a, b coord) int {
+	x := a.x - b.x + 1
+	if x < 0 {
+		x *= -1
+	}
+
+	y := a.y - b.y + 1
+	if y < 0 {
+		y *= -1
+	}
+
+	return x * y
+}
+
+func newPairs(points []coord) (result []pair) {
+	for i := range points {
+		for j := i + 1; j < len(points); j++ {
+			a := points[i]
+			b := points[j]
+			result = append(result, pair{
+				a:    a,
+				b:    b,
+				area: getArea(a, b),
+			})
+		}
+	}
+
+	return result
 }
